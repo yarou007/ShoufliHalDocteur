0b6b681ce923c2b23a5d5c7ad4b90294a26112e0
yarou007 <yacine.hami@esen.tn>
2023-12-09 01:12:24 +0100

    Ankara messi

---
+++ app/src/main/java/com/example/shouflihaldocteurv3/Rendez_vous2.java
@@ -0,0 +1,14 @@
+package com.example.shouflihaldocteurv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rendez_vous2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rendez_vous2);
+    }
+}
